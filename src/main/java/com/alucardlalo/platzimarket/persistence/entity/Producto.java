ffeffbfdfdde49568eb5e11ee59155b8d9806631
Eduardo Cuevas <alucardlalo.1@gmail.com>
2020-09-14 16:59:15 -0700

    create entity and conexion to data base

---
+++ src/main/java/com/alucardlalo/platzimarket/persistence/entity/Producto.java
@@ -0,0 +1,86 @@
+package com.alucardlalo.platzimarket.persistence.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="productos")
+public class Producto {
+
+    @Id //se utilizo para decirle al IDE que es el id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//se utilizo para darle un auto increment con la strategy stablecida en indentity
+    @Column(name= "id_producto")//se utilizo dado que el nombre de la base de datos no coincide con el dado en java y de esta manera de especifica a que columna se refiere
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name="id_categoria")
+    private Integer idCategoria;
+
+    @Column(name="codigo_barras")
+    private String codigoBarras;
+
+    @Column(name="precio_venta")
+    private BigDecimal precioVenta;//en precios siempre se debe usar Big decimal
+
+    @Column(name="cantidad_stock")
+    private Integer cantidadStock;
+
+    private Boolean estado;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public BigDecimal getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(BigDecimal precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
