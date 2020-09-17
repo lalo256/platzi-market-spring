875564a0a2bd77e2bf10bf46a9234d822c8199ae
Eduardo Cuevas <alucardlalo.1@gmail.com>
2020-09-17 14:44:01 -0700

    add MapStruct, create interfase in domain and interaction in tables (in domain and repository)

---
+++ src/main/java/com/alucardlalo/platzimarket/domain/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.alucardlalo.platzimarket.domain.repository;
+
+import com.alucardlalo.platzimarket.domain.Product;
+import java.util.List;
+import java.util.Optional;
+
+
+public interface ProductRepository {
+
+    List<Product> getAll();
+    Optional<List<Product>> getByCategory(int categoryId);
+    Optional<List<Product>> getScaseProducts(int quantity);
+    Optional <Product> getProduct(int productId);
+    Product save(Product product);
+    void delete(int productoId);
+    
+}
