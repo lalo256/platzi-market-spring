8cd389f5738211a1a52c82a6fbe958b5d5bfd1a7
Eduardo Cuevas <alucardlalo.1@gmail.com>
2020-09-17 14:14:10 -0700

    complete Querry methods

--- src/main/java/com/alucardlalo/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/alucardlalo/platzimarket/persistence/ProductoRepository.java
@@ -2,10 +2,11 @@ package com.alucardlalo.platzimarket.persistence;
 
 import com.alucardlalo.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.alucardlalo.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository//anotacion para indicar que se interactua con base de datos
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +20,17 @@ public class ProductoRepository {
 
     public Optional <List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void deleye(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
