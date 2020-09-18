7342bda334f7cb4e6e384b27e70959e6ff1aa03d
Eduardo Cuevas <alucardlalo.1@gmail.com>
2020-09-18 15:22:54 -0700

    inyeccion de dependencias

--- src/main/java/com/alucardlalo/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/alucardlalo/platzimarket/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.alucardlalo.platzimarket.domain.repository.ProductRepository;
 import com.alucardlalo.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.alucardlalo.platzimarket.persistence.entity.Producto;
 import com.alucardlalo.platzimarket.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,8 +13,15 @@ import java.util.Optional;
 
 @Repository//anotacion para indicar que se interactua con base de datos
 public class ProductoRepository implements ProductRepository {
+
+    @Autowired //anotacion para darle a Spring la orden de responsabilizarse de la creacion de las instancias del objeto
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
     private ProductMapper mapper;
+    /*cuando se use autowired se debe estar 100% seguro que el objeto a intanciar
+     * es un objeto de spring al ir a ProductoCrudRepository y ver su extends denota ser un objeto de spring
+     * en caso de ProductMapper no es de spring pero esta epecificado que usa componetModel = spring lo que asegura que se use
+     * aunque este sea un objeto de mapStruct y no de spring*/
 
     @Override
     public List<Product> getAll(){
