0fbd2dc15e7af5896f904d0a4bcb9d3b5952b077
Eduardo Cuevas <alucardlalo.1@gmail.com>
2020-09-22 16:28:06 -0700

    add control de respuestas HTTP en peticiones a API

--- src/main/java/com/alucardlalo/platzimarket/web/controller/ProductController.java
+++ src/main/java/com/alucardlalo/platzimarket/web/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.alucardlalo.platzimarket.web.controller;
 import com.alucardlalo.platzimarket.domain.Product;
 import com.alucardlalo.platzimarket.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,33 +18,42 @@ public class ProductController {
 
     @GetMapping("/all")//se usa para lanzar la api desde la API y con que direcionamiento se accede
     //se entra desde http://localhost:8090/platzi-market/api/products/all
-    public List<Product> getAll(){
-        return productService.getAll();
+    public ResponseEntity<List<Product>> getAll(){
+    //se cambio para controlar las peticiones HTTP
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")//la notacion es para especificar el numero de id y path variable lo especifica
-    public Optional<Product> getProduct(@PathVariable("id") int productId){
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
         //se entra desde http://localhost:8090/platzi-market/api/products/(numero de id)
-        return productService.getProduct(productId);
+        return productService.getProduct(productId).map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        //el metodo orElse se utiliza para el caso en que no exista el id solicitado y responda not found
     }
 
     @GetMapping("/category/{categoryId}")
     //al igual que el anterior se utiliza con una diferencia y es la especificacion de category para que Spring no confunda el de arriba con el de category
     //se entra desde http://localhost:8090/platzi-market/api/category/(numero de id)
-    public Optional <List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
-        return productService.getByCategory(categoryId);
+    public ResponseEntity <List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+        return productService.getByCategory(categoryId)
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))//mapea los resultados
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));//contesta si este no existe
     }
 
     @PostMapping("/save")//salva los datos dentro de la API
     //se entra desde http://localhost:8090/platzi-market/api/save (con una seleccion post en postman)
-    public Product save(@RequestBody Product product){
-        return productService.save(product);
+    public ResponseEntity <Product> save(@RequestBody Product product){
+        return new ResponseEntity<>(productService.save(product),HttpStatus.CREATED);//respuesta http de objeto creado
     }
 
     @DeleteMapping("/delete/{id}")
     //se entra desde http://localhost:8090/platzi-market/api/delete/(numero id del objeto a eliminar) con una seleccion Delete en postman
-    public Boolean delete(@PathVariable("id") int productId){
-        return productService.delete(productId);
+    public ResponseEntity delete(@PathVariable("id") int productId){
+        if(productService.delete(productId)){
+            return new ResponseEntity(HttpStatus.OK);//respuesta si borra
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);//respuesta si no existe
+        }
     }
 
     /*la seccion de save y delete se pueden verificar en postman poniendo el direccionamiento y
