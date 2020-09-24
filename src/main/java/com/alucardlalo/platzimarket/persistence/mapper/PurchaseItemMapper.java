5b62a713285c3afcc91d322d5c530733ef1c62ad
Eduardo Cuevas <alucardlalo.1@gmail.com>
2020-09-24 14:07:47 -0700

    mapper en dominio

---
+++ src/main/java/com/alucardlalo/platzimarket/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,28 @@
+package com.alucardlalo.platzimarket.persistence.mapper;
+
+import com.alucardlalo.platzimarket.domain.PurchaseItem;
+import com.alucardlalo.platzimarket.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface PurchaseItemMapper {
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productoId"),
+            @Mapping(source = "cantidad",target = "quantity"),
+            //no es necesario porner el mapping de total ya que existe exactamente igual em ambos purchase
+            @Mapping(source = "estado" , target = "active")
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto producto);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "compra",ignore = true),
+            @Mapping(target = "producto",ignore = true),
+            @Mapping(target = "id.idCompra",ignore = true)
+    })
+    ComprasProducto toComprasProducto(PurchaseItem item);
+
+}
---
+++ src/main/java/com/alucardlalo/platzimarket/persistence/mapper/PurchaseMapper.java
@@ -0,0 +1,29 @@
+package com.alucardlalo.platzimarket.persistence.mapper;
+
+import com.alucardlalo.platzimarket.domain.Purchase;
+import com.alucardlalo.platzimarket.persistence.entity.Compra;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {PurchaseItemMapper.class})
+public interface PurchaseMapper {
+    @Mappings({
+            @Mapping(source = "idCompra",target = "purchaseId"),
+            @Mapping(source = "idCliente",target = "clientId"),
+            @Mapping(source = "fecha",target = "date"),
+            @Mapping(source = "medioPago", target = "paymentMethod"),
+            @Mapping(source = "comentario", target = "comment"),
+            @Mapping(source = "estado", target = "state"),
+            @Mapping(source = "productos", target = "item")
+    })
+    Purchase toPurchase(Compra compra);
+    List<Purchase> toPurchase(List<Compra> compra);//no necesita un mapper porq ya esta definido arriba y lo toma
+
+    @InheritInverseConfiguration
+    @Mapping(target = "cliente", ignore = true)
+    Compra toCompra(Purchase purchase);
+}
