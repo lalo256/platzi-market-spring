470c3df24a39c2f66fd79baba4d3c969c3738152
Eduardo Cuevas <alucardlalo.1@gmail.com>
2020-09-29 21:45:30 -0700

    protection API with jwt y creacion de token

--- src/main/java/com/alucardlalo/platzimarket/web/security/JWTUtil.java
+++ src/main/java/com/alucardlalo/platzimarket/web/security/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.alucardlalo.platzimarket.web.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,5 +17,22 @@ public class JWTUtil {
         return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())//optencion de contraseña con userDetail y la hora local
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))//vencimiento de contraseña apartir del tiempo local + 10 hr
                 .signWith(SignatureAlgorithm.HS256,key).compact();//firma de algoritmo con formato HS256 y la key declarada
+    }
+
+    //creacion de validacion para el uso del token
+    public boolean validateToken(String token, UserDetails userDetails){
+        return userDetails.getUsername().equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token){
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token){
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token){
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
     }
 }
--- src/main/java/com/alucardlalo/platzimarket/web/security/SecurityConfig.java
+++ src/main/java/com/alucardlalo/platzimarket/web/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.alucardlalo.platzimarket.web.security;
 
 import com.alucardlalo.platzimarket.domain.service.APIUserDetailService;
+import com.alucardlalo.platzimarket.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.persistence.Basic;
 /*interfaz encargada de la seguridad de la API*/
@@ -19,6 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private APIUserDetailService apiUserDetailService;
 
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
+
     //metodo para definir el uso de usuario y contraseña declarados en service.APIUserDetailService
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,7 +35,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()//todas las peticiones que tengan authentica son permitidas
-        .anyRequest().authenticated();//cualquier otra peticion necesita autentificacion
+        .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//se agrego para asignar peticiones al filtro
+        //cualquier otra peticion necesita autentificacion
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
     }
 
     //elecion para gestor de autentificacion
